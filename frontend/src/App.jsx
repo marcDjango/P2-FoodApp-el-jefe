2b813e5b7bb021ec5be30d14c224d4c1eedf7945
THEO NAPOLY <THEO.NAPOLY@gmail.com>
2023-10-31 16:32:39 +0100

    fix(App.jsx file): change first paragraph text

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Coucou</p>
+        <p>Hello</p>
 
         <Counter />
 
