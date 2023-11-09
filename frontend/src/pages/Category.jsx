0be7f5a1725f1a4f26cc1a2b2a51003b632e6ccc
Marcelo <marceloromero2016ar@gmail.com>
2023-11-09 16:29:27 +0100

    fix: delet comment

--- frontend/src/pages/Category.jsx
+++ frontend/src/pages/Category.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import OneCategory from "../components/OneCategory/OneCategory"; // Assurez-vous d'importer correctement votre composant
+import OneCategory from "../components/OneCategory/OneCategory";
 
 function Category() {
   const [categoryParams, setCategoryParams] = useState("");
-  const [key, setKey] = useState(0); // Nouvelle variable d'état pour forcer la mise à jour
+  const [key, setKey] = useState(0);
   const { type } = useParams();
 
   useEffect(() => {
@@ -12,7 +12,6 @@ function Category() {
   }, [type]);
 
   useEffect(() => {
-    // Changez la clé à chaque fois que vous voulez forcer la mise à jour
     setKey((prevKey) => prevKey + 1);
   }, [categoryParams]);
 
