23e6afed86a7e7659566a1fc076296a1e9c9bb66
Marcelo <marceloromero2016ar@gmail.com>
2023-11-16 15:23:35 +0100

    fix: correction request

--- frontend/src/components/Carousel/Carousel.jsx
+++ frontend/src/components/Carousel/Carousel.jsx
@@ -6,9 +6,9 @@ function Carousel() {
   return (
     <>
       <div className="CarouselDesktop">
-        {/* <CarouselSelect mealType="breakfast" />
+        <CarouselSelect mealType="breakfast" />
         <CarouselSelect mealType="dinner" />
-        <CarouselSelect mealType="teatime" /> */}
+        <CarouselSelect mealType="teatime" />
       </div>
       <div className="CarouselMobile">
         <CarouselSelect mealType="lunch" />
