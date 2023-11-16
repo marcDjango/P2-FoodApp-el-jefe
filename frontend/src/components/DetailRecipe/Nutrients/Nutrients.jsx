a9b755917e8e9a53a8d54110425ec4fb5b0b7977
Adri-bis <adrien.ma.16@hotmail.fr>
2023-11-16 09:37:35 +0100

    fix: cleancode added and scss imgdetaill ajust

--- frontend/src/components/DetailRecipe/Nutrients/Nutrients.jsx
+++ frontend/src/components/DetailRecipe/Nutrients/Nutrients.jsx
@@ -7,115 +7,55 @@ import Sugar from "../../../assets/images/Sugar.svg";
 import Fiber from "../../../assets/images/Fiber.svg";
 import Protein from "../../../assets/images/Protein.svg";
 import { useData } from "../../../services/Context";
+import AllNutrients from "./AllNutrients";
 import "./Nutrients.scss";
 
 function Nutrients() {
   const { data } = useData();
 
   return (
-    <>
-      <div className="NutrientsTitle">Nutritional value /pers</div>
-      <div className="Nutrients">
-        <div className="BLockNutri">
-          <img src={Cal} alt="Cal" />
-          <div className="DivInfo">
-            <div>{data.totalNutrients.ENERC_KCAL.label} </div>
-            <div className="NumberNutri">
-              <div>
-                {Math.round(
-                  data.totalNutrients.ENERC_KCAL.quantity / data.yield
-                )}
-              </div>
-              <div>{data.totalNutrients.ENERC_KCAL.unit}</div>
-            </div>
-          </div>
-        </div>
-        <div className="BLockNutri">
-          <img className="FatImg" src={Fat} alt="Fat" />
-          <div className="DivInfo">
-            <div>{data.totalNutrients.FAT.label} </div>
-            <div className="NumberNutri">
-              <div>
-                {Math.round(data.totalNutrients.FAT.quantity / data.yield)}
-              </div>
-              <div>{data.totalNutrients.FAT.unit}</div>
-            </div>
-          </div>
-        </div>
-        <div className="BLockNutri">
-          <img src={Saturated} alt="Saturated" />
-          <div className="DivInfo">
-            <div>{data.totalNutrients.FASAT.label} </div>
-            <div className="NumberNutri">
-              <div>
-                {Math.round(data.totalNutrients.FASAT.quantity / data.yield)}
-              </div>
-              <div>{data.totalNutrients.FASAT.unit}</div>
-            </div>
-          </div>
-        </div>
-        <div className="BLockNutri">
-          <img src={Carbs} alt="Carbs" />
-          <div className="DivInfo">
-            <div>{data.totalNutrients.CHOCDF.label} </div>
-            <div className="NumberNutri">
-              <div>
-                {Math.round(data.totalNutrients.CHOCDF.quantity / data.yield)}
-              </div>
-              <div>{data.totalNutrients.CHOCDF.unit}</div>
-            </div>
-          </div>
-        </div>
-        <div className="BLockNutri">
-          <img src={Sugar} alt="Sugar" />
-          <div className="DivInfo">
-            <div>{data.totalNutrients.SUGAR.label} </div>
-            <div className="NumberNutri">
-              <div>
-                {Math.round(data.totalNutrients.SUGAR.quantity / data.yield)}
-              </div>
-              <div>{data.totalNutrients.SUGAR.unit}</div>
-            </div>
-          </div>
-        </div>
-        <div className="BLockNutri">
-          <img src={Fiber} alt="Fiber" />
-          <div className="DivInfo">
-            <div>{data.totalNutrients.FIBTG.label} </div>
-            <div className="NumberNutri">
-              <div>
-                {Math.round(data.totalNutrients.FIBTG.quantity / data.yield)}
-              </div>
-              <div>{data.totalNutrients.FIBTG.unit}</div>
-            </div>
-          </div>
-        </div>
-        <div className="BLockNutri">
-          <img src={Protein} alt="Protein" />
-          <div className="DivInfo">
-            <div>{data.totalNutrients.PROCNT.label} </div>
-            <div className="NumberNutri">
-              <div>
-                {Math.round(data.totalNutrients.PROCNT.quantity / data.yield)}
-              </div>
-              <div>{data.totalNutrients.PROCNT.unit}</div>
-            </div>
-          </div>
-        </div>
-        <div className="BLockNutri">
-          <img src={Cal} alt="Cal" />
-          <div className="DivInfo">
-            <div>{data.totalNutrients.CHOLE.label} </div>
-            <div className="NumberNutri">
-              <div>
-                {Math.round(data.totalNutrients.CHOLE.quantity / data.yield)}
-              </div>
-              <div>{data.totalNutrients.CHOLE.unit}</div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+    <div className="Nutrients">
+      <AllNutrients
+        NameFood={data.totalNutrients.ENERC_KCAL}
+        NameImg={Cal}
+        NameClassImg="CalImg"
+      />
+      <AllNutrients
+        NameFood={data.totalNutrients.FAT}
+        NameImg={Fat}
+        NameClassImg="FatImg"
+      />
+      <AllNutrients
+        NameFood={data.totalNutrients.FASAT}
+        NameImg={Saturated}
+        NameClassImg="SaturatedImg"
+      />
+      <AllNutrients
+        NameFood={data.totalNutrients.CHOCDF}
+        NameImg={Carbs}
+        NameClassImg="CarbsImg"
+      />
+      <AllNutrients
+        NameFood={data.totalNutrients.SUGAR}
+        NameImg={Sugar}
+        NameClassImg="SugarImg"
+      />
+      <AllNutrients
+        NameFood={data.totalNutrients.FIBTG}
+        NameImg={Fiber}
+        NameClassImg="FiberImg"
+      />
+      <AllNutrients
+        NameFood={data.totalNutrients.PROCNT}
+        NameImg={Protein}
+        NameClassImg="ProteinImg"
+      />
+      <AllNutrients
+        NameFood={data.totalNutrients.CHOLE}
+        NameImg={Cal}
+        NameClassImg="CholesteroleImg"
+      />
+    </div>
   );
 }
 
